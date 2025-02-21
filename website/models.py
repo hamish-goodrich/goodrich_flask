72aa8eb929f7f494f607187a3847542a29892f2f
hamish <hamish@goodrich.nz>
2025-02-21 15:59:53 +1300

    full csv

--- website/models.py
+++ website/models.py
@@ -12,8 +12,8 @@ class Note(db.Model):
 class Monitoring_log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pond_name = db.Column(db.String(400))
-    date = db.Column(db.String(40))
-    time = db.Column(db.String(40))
+    date = db.Column(db.String(50))
+    time = db.Column(db.String(50))
     weather = db.Column(db.String(40))
     temperature = db.Column(db.String(40))
     ibc_level = db.Column(db.Integer)
@@ -26,6 +26,9 @@ class Monitoring_log(db.Model):
     forebay_comments = db.Column(db.String(400))
     forebay_floc_dosed = db.Column(db.Integer)
     forebay_lime_dosed = db.Column(db.Integer)
+    forebay_silt = db.Column(db.Integer)
+    forebay_clear = db.Column(db.Boolean)
+    forebay_cloudy = db.Column(db.Boolean)
 
     main_sample = db.Column(db.Boolean)
     main_ph = db.Column(db.Float)
@@ -35,6 +38,9 @@ class Monitoring_log(db.Model):
     main_comments = db.Column(db.String(400))
     main_floc_dosed = db.Column(db.Integer)
     main_lime_dosed = db.Column(db.Integer)
+    main_silt = db.Column(db.Integer)
+    main_clear = db.Column(db.Boolean)
+    main_cloudy = db.Column(db.Boolean)
 
     flume_flow      = db.Column(db.Boolean)
     forebay_flow    = db.Column(db.Boolean)
@@ -44,6 +50,7 @@ class Monitoring_log(db.Model):
     unit_type = db.Column(db.String(40))
     floc_type = db.Column(db.String(40))
     author = db.Column(db.String(400))
+    company = db.Column(db.String(400))
 
 
 class User(db.Model, UserMixin):
