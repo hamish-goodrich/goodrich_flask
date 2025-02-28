12dd21692f0968c600de61deaa167f0f9634cc37
hamish <hamish@goodrich.nz>
2025-02-28 14:53:38 +1300

    added admin (but not properly)

--- website/auth.py
+++ website/auth.py
@@ -46,6 +46,8 @@ def sign_up():
         full_name = first_name+' '+last_name
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        admin = request.form.get('admin')
+        sudo = request.form.get('sudo')
 
         user = User.query.filter_by(email=email).first()
         if user:
@@ -59,7 +61,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, full_name=full_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(email=email, full_name=full_name, password=generate_password_hash(password1, method='pbkdf2:sha256'), admin=admin, sudo=sudo)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
